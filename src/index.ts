d765f8c812683a4ff83d0c04d591e90cb640192e
Kunal Sharma <kunalsharma19997@gmail.com>
2024-08-04 17:08:12 +0000

    Support for multiple dir options in plugin

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,72 @@
 import { readFile, readdir } from "fs/promises";
+import { existsSync } from "fs";
 
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyBaseLogger } from "fastify";
 
 interface pluginOption {
-  dir: string;
+  dir: string | string[];
 }
 
 const supported_files = ["json"];
 
+let logger: FastifyBaseLogger | undefined = undefined;
+
 const read_file = async (filePath: string) => {
-  const data = await readFile(filePath, {
-    encoding: "utf-8",
-  });
-  return JSON.parse(data);
+  try {
+    const [fileName, ext] = filePath.toLowerCase().split(".");
+
+    if (!fileName || !supported_files.includes(ext)) {
+      logger?.warn(`Unsupported file found : ${filePath}`);
+    }
+    const data = await readFile(filePath, {
+      encoding: "utf-8",
+    });
+
+    return {
+      path: fileName,
+      data: JSON.parse(data),
+    };
+  } catch (err) {
+    logger?.warn(`Could not parse : ${filePath}`);
+
+    return null;
+  }
+};
+
+const load_files = async (directory_path: string) => {
+  if (!existsSync(directory_path)) {
+    logger!.warn(`${directory_path} Directory not exists`);
+    return;
+  }
+  const files = await readdir(`${directory_path}`);
+  return Promise.all(
+    files.map((filePath) => read_file(`${directory_path}/${filePath}`))
+  );
 };
 
 const autoLoad = async (fastify: FastifyInstance, options: pluginOption) => {
-  let result = [];
-  try {
-    const files = await readdir(`${options.dir}`);
+  let { dir } = options;
 
-    result = files.map(async (file) => {
-      const [fileName, ext] = file.toLowerCase().split(".");
+  logger = fastify.log || console;
 
-      const result = {
-        path: `${options.dir}/${fileName}`,
-        data: null,
-      };
-
-      try {
-        if (fileName && supported_files.includes(ext)) {
-          result.data = await read_file(`${options.dir}/${file}`);
-        } else {
-          fastify.log.warn(`Unsupported file found : ${file}`);
-        }
-      } catch (err) {
-        fastify.log.warn(`Could not parse : ${file}`);
-      }
-
-      return result;
-    });
-
-    result = await Promise.all(result);
-
-    result.forEach(({ path, data }) => {
-      if (data) {
-        fastify.get(`/${path}`, (_, reply) => {
-          reply.send(data);
-        });
-      }
-    });
-  } catch (_) {
-    fastify.log.warn(`Directory not found : ${options.dir}`);
+  if (!dir) {
+    logger?.warn("Nothing to load");
+    return;
   }
+
+  if (!Array.isArray(dir)) dir = [dir];
+
+  const result = await Promise.all(dir.map((path) => load_files(path)));
+
+  result
+    .flat()
+    .filter((_item) => _item !== null && _item !== undefined)
+    .forEach(({ path, data }) =>
+      fastify.get(`/${path}`, (_, reply) => {
+        logger?.debug(`Loading path : ${path}`);
+        reply.send(data);
+      })
+    );
 };
 
 export default autoLoad;
