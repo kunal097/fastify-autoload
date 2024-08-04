dd4f5e80943de3d0620196705f9cf4c88bdcf8d4
Kunal Sharma <kunalsharma19997@gmail.com>
2024-08-04 13:27:10 +0000

    tests added for supporting multiple path in dir option while registering module.

--- __tests__/plugin.test.ts
+++ __tests__/plugin.test.ts
@@ -1,13 +1,11 @@
 import Fastify from "fastify";
 import autoLoad from "../src";
 
-const build = (path: string) => {
+const build = (options: { dir: string; prefix?: string }) => {
   const app = Fastify();
 
   beforeAll(async () => {
-    void app.register(autoLoad, {
-      dir: path,
-    });
+    void app.register(autoLoad, options);
     await app.ready();
   });
 
@@ -17,7 +15,9 @@ const build = (path: string) => {
 };
 
 describe("Autoload plugin test suit for existing dir", () => {
-  const app = build("__tests__/files");
+  const app = build({
+    dir: "__tests__/files",
+  });
   test("Check for server route", async () => {
     const res = await app.inject({
       url: "__tests__/files/server",
@@ -36,8 +36,52 @@ describe("Autoload plugin test suit for existing dir", () => {
   });
 });
 
+describe("Autoload plugin test suit for multiple directory", () => {
+  const app = build({
+    //@ts-ignore
+    dir: ["__tests__/files", "__tests__/config"],
+  });
+
+  test("Check for server route from files directory", async () => {
+    const res = await app.inject({
+      url: "__tests__/files/server",
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toEqual({ name: "server config", env: "test" });
+  });
+
+  test("Check for server route from config directory", async () => {
+    const res = await app.inject({
+      url: "__tests__/config/server",
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toEqual({
+      name: "server config from __tests__/config",
+      env: "test",
+    });
+  });
+
+  test("Check for invalid route from files directroy", async () => {
+    const res = await app.inject({
+      url: "api/config/__tests__/files/invalid",
+    });
+
+    expect(res.statusCode).toBe(404);
+  });
+
+  test("Check for invalid route from config directroy", async () => {
+    const res = await app.inject({
+      url: "api/config/__tests__/config/invalid",
+    });
+
+    expect(res.statusCode).toBe(404);
+  });
+});
+
 describe("Autoload plugin test suit for non-existent directory", () => {
-  const app = build("__tests__/non-existent");
+  const app = build({
+    dir: "__tests__/non-existent",
+  });
   test("Check for server route", async () => {
     const res = await app.inject({
       url: "__tests__/files/server",
@@ -46,3 +90,5 @@ describe("Autoload plugin test suit for non-existent directory", () => {
     expect(res.statusCode).toBe(404);
   });
 });
+
+export default build;
